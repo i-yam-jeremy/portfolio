60c13bc40061d2c5c0edfc2c8d5d94d10e7903d7
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-12-03 21:01:19 -0800

    Added guiding lines to home

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ function selectPage(id) {
   }
 }
 
+function updateHomeGuidingLines() {
+
+  const header = document.getElementById("home-header");
+
+  const topLeftPoint = [window.innerWidth/2 - header.clientWidth/2, window.innerHeight/2 - header.clientHeight/2];
+
+  for (const svgElement of document.getElementsByClassName("home-guiding-line")) {
+    const polygon = svgElement.children[0];
+    polygon.setAttribute('points', `0,0 ${topLeftPoint[0] + 20},${topLeftPoint[1]} ${topLeftPoint[0]},${topLeftPoint[1] + 20}`);
+    svgElement.setAttribute('width', window.innerWidth/2);
+    svgElement.setAttribute('height', window.innerHeight/2);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   selectPage('home');
+  updateHomeGuidingLines();
+});
+
+window.addEventListener('resize', () => {
+  updateHomeGuidingLines();
 });
